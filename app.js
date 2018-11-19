f5e8ec3803e6d5aa1dcab7c0ff5b8671d4b886b3
Phung Pham <phungpham.tech@gmail.com>
2018-11-19 16:16:21 -0500

    updated gifphy url

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
         $(this).addClass('active');
 
         var type= $(this).attr('data-type');
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=vcFSZHIeOGB4Tn4z9NPhkS50gxoyUNMl&limit=10";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=vcFSZHIeOGB4Tn4z9NPhkS50gxoyUNMl&limit=10";
          
         $.ajax({
             url: queryURL,
