b2261695dd8f5015a13c0083c0e50c1081c98959
Phung Pham <phungpham.tech@gmail.com>
2018-11-19 15:56:23 -0500

    completed this app

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# animal-gifs
+#Animal Gifphy Stastic
+
+### Overview
+This app allows users to add animal's name and search related images using Gifphy API.
+
+Link to use this app: https://phungdpham.github.io/animal-gifstatic/
+
+### Technologies
+
+* html
+* css
+* javascript
+* jQuery
+* ajax
--- app.js
+++ app.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     function renderBtn(arrToUse, classToAdd, areaToAddTo) {
         $(areaToAddTo).empty();
 
-        for(var i = 0; i<arrToUse.length; i++) {
+        for(var i = 0; i < arrToUse.length; i++) {
             var a = $('<button>');
             a.addClass(classToAdd);
             a.attr('data-type', arrToUse[i]);
@@ -21,21 +21,23 @@ $(document).ready(function() {
         $(this).addClass('active');
 
         var type= $(this).attr('data-type');
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=dc6zaTOxFJmzC&limit=10";
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=vcFSZHIeOGB4Tn4z9NPhkS50gxoyUNMl&limit=10";
          
         $.ajax({
             url: queryURL,
-            method: 'Get'
+            method: 'GET'
         }).then(function(response) {
             var results = response.data;
-            for (var i = 0; i < results.length; i++); {
+
+            for (var i = 0; i < results.length; i++) {
 
                 var animalDiv = $("<div class=\"animal-item\">");
 
                 var rating = results[i].rating;
                 var p = $('<p>').text('Rating: ' + rating);
+
                 var animated = results[i].images.fixed_height.url;
-                var still = results[i].images.fixed-height_still.url;
+                var still = results[i].images.fixed_height_still.url;
 
                 var animalImage = $('<img>');
                 animalImage.attr('src', still);
@@ -67,8 +69,8 @@ $(document).ready(function() {
         if (newAnimal.length > 2) {
             animals.push(newAnimal);
         }
-        renderBtn(animal, 'animal-button', '#animal-buttons');
+        renderBtn(animals, 'animal-button', '#animal-buttons');
     });
-    renderBtn(animal, 'animal-button', '#animal-buttons');
+    renderBtn(animals, 'animal-button', '#animal-buttons');
 
 });
